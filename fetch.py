c9d4db5b55e09b437d42192584ebe7ef334af1c5
Nick Young <nick.young@auckland.ac.nz>
2020-09-09 16:33:34 +1200

    use requests to workaround 403

--- fetch.py
+++ fetch.py
@@ -30,7 +30,7 @@ last_modified = last_modified[last_modified.find(search_string) + len(search_str
 with open("last_modified.txt", "w") as f:
     f.write(last_modified)
 print(last_modified)
-df = pd.read_excel(link, skiprows=2, sheet_name=None, skip_blank_lines=True)
+df = pd.read_excel(requests.get(link).content, skiprows=2, sheet_name=None, skip_blank_lines=True)
 for k,v in df.items():
     v["Case Type"] = k
 df = pd.concat(df.values())
