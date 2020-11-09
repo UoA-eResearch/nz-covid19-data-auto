59abfa57ac815c2b1f878dd32fa35efced52f2b7
Nick <nick.young@auckland.ac.nz>
2020-11-10 12:57:16 +1300

    index=False

--- fetch_ESR.py
+++ fetch_ESR.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 from io import StringIO
 
-session = "93847a240fe248bf634f298ec2410d2f"
+session = "33e740e66165c55ca28324082a74b88d"
 cookie = "route=1604958355.109.13309.111578"
 
 r = requests.get(f"https://nzcoviddashboard.esr.cri.nz/session/{session}/download/overview-downloadCurveData", headers={
@@ -16,4 +16,4 @@ df = pd.read_csv(data, skiprows=3, skip_blank_lines=True)
 df = df.dropna()
 print(df)
 
-df.to_csv("cases_by_DHB_over_time.csv")
+df.to_csv("cases_by_DHB_over_time.csv", index=False)
