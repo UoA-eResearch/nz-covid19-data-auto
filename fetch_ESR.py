afa4b09c200793e9535a51749511be44045d2699
Nick <nick.young@auckland.ac.nz>
2020-11-10 15:23:53 +1300

    pd.to_datetime

--- fetch_ESR.py
+++ fetch_ESR.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 from io import StringIO
 
-session = "33e740e66165c55ca28324082a74b88d"
+session = "30ab9ebcd25b51fa3e9f722a9a95c35f"
 cookie = "route=1604958355.109.13309.111578"
 
 r = requests.get(f"https://nzcoviddashboard.esr.cri.nz/session/{session}/download/overview-downloadCurveData", headers={
@@ -14,6 +14,7 @@ r = requests.get(f"https://nzcoviddashboard.esr.cri.nz/session/{session}/downloa
 data = StringIO(r.text)
 df = pd.read_csv(data, skiprows=3, skip_blank_lines=True)
 df = df.dropna()
+df.Date = pd.to_datetime(df.Date, dayfirst=True)
 print(df)
 
 df.to_csv("cases_by_DHB_over_time.csv", index=False)
