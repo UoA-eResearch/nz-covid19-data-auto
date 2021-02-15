35202942d66d54bfc2a0a0cebb04dea1d566043e
Nick Young <nick.young@auckland.ac.nz>
2021-02-15 16:47:57 +1300

    set author for commits

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ git pull
 python3 fetch.py
 #python3 fetch_ESR.py
 python3 fetch_cases_by_DHB.py
-git commit -am "auto update to $(cat last_modified.txt)"
+git commit -am "auto update to $(cat last_modified.txt)" --author "nz-covid-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
