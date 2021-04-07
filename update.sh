ed1b795130c5d038f39d1ba99a62f899e9e30f61
Nick Young <nick.young@auckland.ac.nz>
2021-04-07 21:48:23 +1200

    auto track vaccinations

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ git pull
 python3 fetch.py
 #python3 fetch_ESR.py
 python3 fetch_cases_by_DHB.py
+python3 fetch_vaccinations.py
 git commit -am "auto update to $(cat last_modified.txt)" --author="nz-covid-bot <ubuntu@elevation.auckland-cer.cloud.edu.au"
 git push
