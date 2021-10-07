e9c23d0055e8bde56da7427762d84c6f98e52ca8
Nick Young <nick.young@auckland.ac.nz>
2021-10-08 12:40:39 +1300

    add fetch_healthpoint and csv_to_geojson to update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,7 @@ python3 fetch.py
 #python3 fetch_ESR.py
 python3 fetch_cases_by_DHB.py
 python3 fetch_vaccinations.py
+python3 fetch_healthpoint.py
+python3 csv_to_geojson.py
 git commit -am "auto update to $(cat last_modified.txt)" --author="nz-covid-bot <ubuntu@elevation.auckland-cer.cloud.edu.au"
 git push
