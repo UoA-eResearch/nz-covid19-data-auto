8a5d7a1392e0bd966165372566fc1628b517bc20
Nick Young <nick.young@auckland.ac.nz>
2021-10-08 19:06:16 +1300

    Sort by just lat and lng

--- fetch_healthpoint.py
+++ fetch_healthpoint.py
@@ -11,5 +11,5 @@ locations = requests.get("https://www.healthpoint.co.nz/geo.do?zoom=18&branch=co
 for l in locations:
     l["url"] = "https://www.healthpoint.co.nz" + l["url"]
 print(len(locations))
-df = pd.DataFrame(locations).sort_values(by=["id", "lat", "lng"])
+df = pd.DataFrame(locations).sort_values(by=["lat", "lng"])
 df.to_csv("vaccinations/healthpoint_locations.csv", index=False)
