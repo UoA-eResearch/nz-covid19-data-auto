94f8b5bdce7674655c89f002e273153a31cae506
Nick Young <nick.young@auckland.ac.nz>
2021-10-18 09:49:08 +1300

    ignore minor changes, such as change of id these are probably caused by colocated services, and the API chooses one randomly, resulting in flapping changes note that sometimes the branch was changing, such as in the case of a colocated pharmacy and primary healthcare provider this should make for a tidier commit history

--- fetch_healthpoint.py
+++ fetch_healthpoint.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 import requests
-import os
-import json
-from pprint import pprint
 import pandas as pd
-import time
+import numpy as np
+
+FILENAME = "vaccinations/healthpoint_locations.csv"
+existing = pd.read_csv(FILENAME)
 
 locations = requests.get("https://www.healthpoint.co.nz/geo.do?zoom=18&branch=covid-19-vaccination").json()["results"]
-for l in locations:
+for i, l in enumerate(locations):
     l["url"] = "https://www.healthpoint.co.nz" + l["url"]
-    if l["id"] == 464094:
-        l["id"] = 318617
-        l["branch"] = "primary"
+    existing_location_different_id = existing[
+        np.isclose(existing.lat, l["lat"]) &
+        np.isclose(existing.lng, l["lng"]) &
+        (existing.name == l["name"]) &
+        (existing.url == l["url"]) &
+        (existing.id != l["id"])
+    ]
+    if len(existing_location_different_id):
+        print(f"Ignoring minor change:\n{existing_location_different_id.to_dict(orient='records')[0]} ->\n{l}")
+        l["id"] = existing_location_different_id.id.iloc[0]
+        l["branch"] = existing_location_different_id.branch.iloc[0]
 print(len(locations))
 df = pd.DataFrame(locations).sort_values(by=["id", "lat", "lng"])
 df.to_csv("vaccinations/healthpoint_locations.csv", index=False)
