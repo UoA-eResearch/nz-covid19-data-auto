830678917abaf5b951202dc00a3f7ae01c828df6
Nick Young <nick.young@auckland.ac.nz>
2021-10-18 15:47:26 +1300

    auto fetch bookmyvaccine locations

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ python3 fetch.py
 python3 fetch_cases_by_DHB.py
 python3 fetch_vaccinations.py
 python3 fetch_healthpoint.py
+python3 fetch_bookmyvaccine.py
 python3 csv_to_geojson.py
 git commit -am "auto update to $(cat last_modified.txt)" --author="nz-covid-bot <ubuntu@elevation.auckland-cer.cloud.edu.au"
 git push
